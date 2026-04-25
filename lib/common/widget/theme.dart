34f37f752013aeb56f4922f8478b48aa92983c8a
now-0o <dlwpdn0704@gmail.com>
2026-04-25 15:05:34 +0900

    fead : Add theme 추가
    일부 color token 추가
    
    Ref : #29

---
+++ lib/common/widget/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color bgBase = Color(0xFFF4F5F6);
+
+  static const Color textPrimary = Color(0xFF1E2124);
+  static const Color textSecondary = Color(0xFF58616A);
+  static const Color textTertiary = Color(0xFF8A949E);
+  static const Color textDisabled = Color(0xFFCDD1D5);
+  static const Color textPrimaryInverse = Color(0xFFFFFFFF);
+
+  static const Color borderDefault = Color(0xFFCDD1D5);
+  static const Color borderLight = Color(0xFFE6E8EA);
+
+  static const Color primary = Color(0xFF6636FF);
+  static const Color primaryVariant = Color(0xFF5913FF);
+  static const Color primarySoft = Color(0xFFF3EEFF);
+  static const Color primaryBorder = Color(0xFFC2A8FF);
+
+  static const Color violet900 = Color(0xFF22066A);
+  static const Color violet800 = Color(0xFF320A94);
+  static const Color violet700 = Color(0xFF440EC3);
+  static const Color violet600 = Color(0xFF5913FF);
+  static const Color violet500 = Color(0xFF6636FF);
+  static const Color violet400 = Color(0xFF8551FF);
+  static const Color violet300 = Color(0xFFA37DFF);
+  static const Color violet200 = Color(0xFFC2A8FF);
+  static const Color violet100 = Color(0xFFE0D4FF);
+  static const Color violet50 = Color(0xFFF3EEFF);
+
+  static const Color gray100 = Color(0xFF000000);
+  static const Color gray95 = Color(0xFF131416);
+  static const Color gray90 = Color(0xFF1E2124);
+  static const Color gray80 = Color(0xFF33363D);
+  static const Color gray70 = Color(0xFF464C53);
+  static const Color gray60 = Color(0xFF58616A);
+  static const Color gray50 = Color(0xFF6D7882);
+  static const Color gray40 = Color(0xFF8A949E);
+  static const Color gray30 = Color(0xFFB1B8BE);
+  static const Color gray20 = Color(0xFFCDD1D5);
+  static const Color gray10 = Color(0xFFE6E8EA);
+  static const Color gray5 = Color(0xFFF4F5F6);
+  static const Color gray0 = Color(0xFFFFFFFF);
+}
