e03ace74b800afe0c8724027c7ed8ef94ec60bc1
now-0o <dlwpdn0704@gmail.com>
2026-05-17 15:26:41 +0900

    feat : 메인화면 개발
    메인 기본 구조 개발
    캘린터 개발 중 중간 커밋
    
    Ref : #36

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'features/test/view/test_page.dart';
+import 'package:front_mobile/features/test/view/home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: TestPage());
+    return const MaterialApp(home: HomePage());
   }
 }
