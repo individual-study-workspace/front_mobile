572945343eaab57c633a9aba40159b74923ddffe
신정희 <junghee6859@gmail.com>
2026-06-02 23:32:33 +0900

    KAN-45 : 서비스 이용 약관 화면 UI
    서비스 이용 약관 화면 UI 생성
    * 라우팅이랑 약관에 대한 설명 화면은 아직 안한 상태

--- lib/features/auth/model/sign_up_state.dart
+++ lib/features/auth/model/sign_up_state.dart
@@ -4,11 +4,40 @@ class SignUpState {
   final String email;
   final String? role;
 
-  const SignUpState({this.name = '', this.email = '', this.role});
+  /// 약관 동의
+  final bool isAdult;
+  final bool agreeTermsOfService;
+  final bool agreePrivacyPolicy;
+  final bool agreeMarketing;
+
+  const SignUpState({
+    this.name = '',
+    this.email = '',
+    this.role,
+
+    this.isAdult = false,
+    this.agreeTermsOfService = false,
+    this.agreePrivacyPolicy = false,
+    this.agreeMarketing = false,
+  });
 
   /// 사용자 프로필 설정
   bool get isUserProfileValid {
     return name.trim().isNotEmpty && email.trim().isNotEmpty && role != null;
+  }
+
+  /// 서비스 이용 약관
+  /// 동의 후 넘어가기
+  bool get isTermsAgreed {
+    return isAdult && agreeTermsOfService && agreePrivacyPolicy;
+  }
+
+  ///전체 동의하기
+  bool get isAllAgreements {
+    return isAdult &&
+        agreeTermsOfService &&
+        agreePrivacyPolicy &&
+        agreeMarketing;
   }
 
   static const _empty = Object();
@@ -27,6 +56,11 @@ class SignUpState {
       name: name ?? this.name,
       email: email ?? this.email,
       role: role == _empty ? this.role : role as String?,
+
+      isAdult: isAdult ?? this.isAdult,
+      agreeTermsOfService: agreeTermsOfService ?? this.agreeTermsOfService,
+      agreePrivacyPolicy: agreePrivacyPolicy ?? this.agreePrivacyPolicy,
+      agreeMarketing: agreeMarketing ?? this.agreeMarketing,
     );
   }
 }
--- lib/features/auth/provider/sign_up_provider.dart
+++ lib/features/auth/provider/sign_up_provider.dart
@@ -24,4 +24,29 @@ class SignUpNotifier extends StateNotifier<SignUpState> {
       state = state.copyWith(role: value);
     }
   }
+
+  void setAdult(bool value) {
+    state = state.copyWith(isAdult: value);
+  }
+
+  void setAgreeTermsOfService(bool value) {
+    state = state.copyWith(agreeTermsOfService: value);
+  }
+
+  void setAgreePrivacyPolicy(bool value) {
+    state = state.copyWith(agreePrivacyPolicy: value);
+  }
+
+  void setAgreeMarketing(bool value) {
+    state = state.copyWith(agreeMarketing: value);
+  }
+
+  void setAllAgreements(bool value) {
+    state = state.copyWith(
+      isAdult: value,
+      agreeTermsOfService: value,
+      agreePrivacyPolicy: value,
+      agreeMarketing: value,
+    );
+  }
 }
