5362e23cb8b1f89e3a2ece0e56d9f4781646d0b0
신정희 <junghee6859@gmail.com>
2026-06-09 00:00:20 +0900

    KAN-46 : Flutter router 설정
    router 설정
    임시로 약관동의 화면으로 초기 라우터 진행

---
+++ lib/app/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front_mobile/app/route_path.dart';
+import 'package:front_mobile/features/auth/view/profile_setup_page.dart';
+import 'package:front_mobile/features/auth/view/terms_agreement_page.dart';
+import 'package:go_router/go_router.dart';
+
+final goRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    ///일단 이렇게 진행 이후에 home으로 변경
+    initialLocation: RoutePath.termsAgreement,
+    routes: [
+      GoRoute(
+        path: RoutePath.termsAgreement,
+        builder: (_, __) => const TermsAgreementPage(),
+      ),
+      GoRoute(
+        path: RoutePath.profileSetup,
+        builder: (_, __) => const ProfileSetupPage(),
+      ),
+    ],
+  );
+});
