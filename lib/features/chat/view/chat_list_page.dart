f7d038a11eb22dc379820a66a8ab1a54060868bf
now-0o <dlwpdn0704@gmail.com>
2026-06-14 14:34:32 +0900

    KAN-48 : 채팅 목록 UI
    기본 세팅

---
+++ lib/features/chat/view/chat_list_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:front_mobile/common/widget/main_app_bar.dart';
+
+class ChatListPage extends StatelessWidget {
+  const ChatListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
