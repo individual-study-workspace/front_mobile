87667832852d2e8881c353d777f268619a9f702c
now-0o <dlwpdn0704@gmail.com>
2026-06-14 15:10:49 +0900

    KAN-48 : 채팅 목록 UI
    채팅 목록 UI 추가

--- lib/features/chat/view/chat_list_page.dart
+++ lib/features/chat/view/chat_list_page.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:front_mobile/common/theme.dart';
 import 'package:front_mobile/common/widget/main_app_bar.dart';
+import 'package:front_mobile/common/widget/text_input.dart';
+import 'package:front_mobile/features/chat/view/chat_list_card.dart';
 
 class ChatListPage extends StatelessWidget {
   const ChatListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(52),
+        child: MainAppBar(title: '채팅'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextInput(
+              size: TextInputSize.small,
+              hintText: '튜터 또는 강의실명을 검색해보세요',
+              leftIcon: SvgPicture.asset(
+                'assets/icons/search_outline.svg',
+                width: 40,
+                height: 40,
+                fit: BoxFit.contain,
+                colorFilter: ColorFilter.mode(
+                  Palette.iconSecondary,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ChatListCard(),
+          ],
+        ),
+      ),
+    );
   }
 }
