d8d3fb86ee2287064b728757292df6d2846ffaee
now-0o <dlwpdn0704@gmail.com>
2026-06-23 20:36:29 +0900

    KAN-48 : 채팅 목록 UI
    검색창 공통 위젯으로 변경

--- lib/features/chat/view/chat_list_page.dart
+++ lib/features/chat/view/chat_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:front_mobile/common/theme.dart';
+import 'package:front_mobile/common/widget/common_search_bar.dart';
 import 'package:front_mobile/common/widget/main_app_bar.dart';
 import 'package:front_mobile/common/widget/text_input.dart';
 import 'package:front_mobile/features/chat/view/chat_list_card.dart';
@@ -20,20 +21,7 @@ class ChatListPage extends StatelessWidget {
         child: Column(
           children: [
             // 검색 창
-            TextInput(
-              size: TextInputSize.small,
-              hintText: '튜터 또는 강의실명을 검색해보세요',
-              leftIcon: SvgPicture.asset(
-                'assets/icons/search_outline.svg',
-                width: 40,
-                height: 40,
-                fit: BoxFit.contain,
-                colorFilter: ColorFilter.mode(
-                  Palette.iconSecondary,
-                  BlendMode.srcIn,
-                ),
-              ),
-            ),
+            CommonSearchBar(hintText: '튜터 또는 강의실명을 검색해보세요'),
             SizedBox(height: 16),
             // api 연동 시 map으로 반복문 돌려서 뿌림
             ChatListCard(),
